bf1142a1f4f9d42d0cced46c0cc12b3c69520b76
Gm_Abhishek <gmabhishek2004@gmail.com>
2022-12-26 23:20:01 +0530

    Off to Salem

---
+++ Layer1.js
@@ -0,0 +1,49 @@
+
+let mybutton = document.getElementById("myBtn");
+let list = document.getElementById("list");
+let logo = document.getElementById("smaag");
+let head = document.getElementById("head-1");
+let r = document.querySelector(':root');
+window.onload = function(){topFunction()};
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+  {
+    mybutton.style.display ="block";
+    list.style.margin = "0.2em 0 0.2em 0";   
+    logo.style.padding= "0.3em 0 0 5em";   
+    logo.style.maxHeight = "3em";
+   head.style.backgroundColor = "white"; 
+   r.style.setProperty('--x', 'black');
+  } 
+  else
+  {
+    mybutton.style.display = "none";    
+    list.style.margin = "1em 0 1em 0";    
+    head.style.backgroundColor = "transparent";
+    head.style.color = "white";
+    logo.style.padding= "1em 0 0 5em";   
+    logo.style.maxHeight = "4em";
+    r.style.setProperty('--x', 'white');
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (var i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
--- index.js
+++ index.js
@@ -11,18 +11,35 @@ function scrollFunction() {
     mybutton.style.display ="block";
     list.style.margin = "0.2em 0 0.2em 0";   
     logo.style.padding= "0.3em 0 0 5em";   
-    logo.style.maxHeight = "2em";
+    logo.style.maxHeight = "3em";
    head.style.backgroundColor = "white"; 
   } 
   else {
     mybutton.style.display = "none";    
     list.style.margin = "1em 0 1em 0";    
-    head.style.background = "transparent";
+    head.style.backgroundColor = "transparent";
+    head.style.color = "white";
     logo.style.padding= "1em 0 0 5em";   
-    logo.style.maxHeight = "3em";
+    logo.style.maxHeight = "4em";
   }
 }
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (var i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
